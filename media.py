e28bd246bd567206120db1effbbadea92846d01b
evertonjunior1-lab <everton.junior1@escolar.ifrn.edu.br>
2026-05-22 19:18:41 +0000

    media.py ok

---
+++ media.py
@@ -0,0 +1,44 @@
+# SISTEMA DE MÉDIA DE ALUNOS
+
+# Função para calcular a média
+def calcular_media(n1, n2):
+    media = (n1 + n2) / 2
+    return media
+
+
+# Função para verificar status
+def verificar_status(media):
+    if media >= 70:
+        return "Aprovado"
+    elif media >= 50:
+        return "Recuperação"
+    else:
+        return "Reprovado"
+
+
+# Função principal
+def main():
+    print("----- Sistema de Cálculo de Média -----")
+
+    continuar = "s"
+
+    while continuar == "s":
+
+        nome = input("Digite o nome do aluno: ")
+
+        nota1 = float(input("Digite a primeira nota: "))
+        nota2 = float(input("Digite a segunda nota: "))
+
+        media = calcular_media(nota1, nota2)
+
+        status = verificar_status(media)
+
+        print("\n------ RESULTADO -------")
+        print(f"Aluno: {nome}")
+        print(f"Média: {media:.2f}")
+        print(f"Status: {status}")
+
+        continuar = input("\nDeseja cadastrar outro aluno? (s/n): ").lower()
+
+
+main()
